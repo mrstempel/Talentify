6376fac398eddc44644278f0f292728e9d18353f
David Stempel <david@slash.co.at>
2015-06-02 10:16:08 +0200

    talentecheck rausnehmen

--- VisualStudio/Talentify.Web/Controllers/TalenteCheckController.cs
+++ VisualStudio/Talentify.Web/Controllers/TalenteCheckController.cs
@@ -11,110 +11,110 @@ namespace Talentify.Web.Controllers
 {
 	public class TalenteCheckController : TalentecheckBaseController
     {
-		[AllowAnonymous]
-        public ActionResult Index()
-        {
-			if (!string.IsNullOrEmpty(Request["s"]))
-			{
-				Guid inviteGuid;
-				if (Guid.TryParse(Request["s"], out inviteGuid))
-				{
-					Session["TalentecheckInviteGuid"] = inviteGuid;
-				}
-			}
+		//[AllowAnonymous]
+		//public ActionResult Index()
+		//{
+		//	if (!string.IsNullOrEmpty(Request["s"]))
+		//	{
+		//		Guid inviteGuid;
+		//		if (Guid.TryParse(Request["s"], out inviteGuid))
+		//		{
+		//			Session["TalentecheckInviteGuid"] = inviteGuid;
+		//		}
+		//	}
 
 
-            return View();
-        }
+		//	return View();
+		//}
 
-		[AllowAnonymous]
-		public ActionResult Umfrage(int f = 1)
-		{
-			ViewBag.Frage = f;
-			return View();
-		}
+		//[AllowAnonymous]
+		//public ActionResult Umfrage(int f = 1)
+		//{
+		//	ViewBag.Frage = f;
+		//	return View();
+		//}
 
-		[AllowAnonymous]
-		public ActionResult Frage(int f = 1)
-		{
-			ViewBag.Frage = f;
-			Type t = TalentecheckSession.GetType();
-			ViewBag.Answer = Convert.ToInt16(t.GetProperty("Frage" + f).GetValue(TalentecheckSession));
-			return View();
-		}
+		//[AllowAnonymous]
+		//public ActionResult Frage(int f = 1)
+		//{
+		//	ViewBag.Frage = f;
+		//	Type t = TalentecheckSession.GetType();
+		//	ViewBag.Answer = Convert.ToInt16(t.GetProperty("Frage" + f).GetValue(TalentecheckSession));
+		//	return View();
+		//}
 
-		[AllowAnonymous]
-		public JsonResult SetAnswer(int f, int a)
-		{
-			if (UnitOfWork.TalentecheckSessionRepository.SetAnswer(TalentecheckSession, f, a))
-			{
-				if (f == 10)
-				{
-					// set finished
-					UnitOfWork.TalentecheckSessionRepository.SetFinished(TalentecheckSession);
-				}
+		//[AllowAnonymous]
+		//public JsonResult SetAnswer(int f, int a)
+		//{
+		//	if (UnitOfWork.TalentecheckSessionRepository.SetAnswer(TalentecheckSession, f, a))
+		//	{
+		//		if (f == 10)
+		//		{
+		//			// set finished
+		//			UnitOfWork.TalentecheckSessionRepository.SetFinished(TalentecheckSession);
+		//		}
 
-				UnitOfWork.Save();
-				return Json(true, JsonRequestBehavior.AllowGet);
-			}
+		//		UnitOfWork.Save();
+		//		return Json(true, JsonRequestBehavior.AllowGet);
+		//	}
 
-			return Json(false, JsonRequestBehavior.AllowGet);
-		}
+		//	return Json(false, JsonRequestBehavior.AllowGet);
+		//}
 
-		[AllowAnonymous]
-		public ActionResult Result()
-		{
-			if (!TalentecheckSession.IsFinished)
-			{
-				return RedirectToAction("Index");
-			}
+		//[AllowAnonymous]
+		//public ActionResult Result()
+		//{
+		//	if (!TalentecheckSession.IsFinished)
+		//	{
+		//		return RedirectToAction("Index");
+		//	}
 
-			var resultPageData = new ResultPageData();
+		//	var resultPageData = new ResultPageData();
 
-			resultPageData.PlusType = TalentecheckSession.TypMax.ToString();
-			resultPageData.PlusTypeReadable = UnitOfWork.TalentecheckSessionRepository.GetTypReadable(TalentecheckSession.TypMax);
-			resultPageData.PlusPercent = TalentecheckSession.MaxPercent;
-			resultPageData.MinusType = TalentecheckSession.TypMin.ToString();
-			resultPageData.MinusTypeReadable = UnitOfWork.TalentecheckSessionRepository.GetTypReadable(TalentecheckSession.TypMin);
-			resultPageData.MinusPercent = TalentecheckSession.MinPercent;
-			resultPageData.Sex = TalentecheckSession.Frage10;
+		//	resultPageData.PlusType = TalentecheckSession.TypMax.ToString();
+		//	resultPageData.PlusTypeReadable = UnitOfWork.TalentecheckSessionRepository.GetTypReadable(TalentecheckSession.TypMax);
+		//	resultPageData.PlusPercent = TalentecheckSession.MaxPercent;
+		//	resultPageData.MinusType = TalentecheckSession.TypMin.ToString();
+		//	resultPageData.MinusTypeReadable = UnitOfWork.TalentecheckSessionRepository.GetTypReadable(TalentecheckSession.TypMin);
+		//	resultPageData.MinusPercent = TalentecheckSession.MinPercent;
+		//	resultPageData.Sex = TalentecheckSession.Frage10;
 
-			// was können die anderen
-			resultPageData.TypeOverview = UnitOfWork.TalentecheckSessionRepository.GetTypeOverview();
+		//	// was können die anderen
+		//	resultPageData.TypeOverview = UnitOfWork.TalentecheckSessionRepository.GetTypeOverview();
 
-			var highscores = (from h in UnitOfWork.TalentecheckHighscoreRepository.AsQueryable()
-				join
-					tc in UnitOfWork.TalentecheckSessionRepository.AsQueryable() on h.TalentecheckSessionId equals tc.Id
-				join user in UnitOfWork.BaseUserRepository.AsQueryable() on tc.UserId equals user.Id
-				
-				select new TalentecheckHighscoreItem()
-				{
-					Firstname = user.Firstname,
-					Surname = user.Surname,
-					Points = h.Points
-				}).OrderByDescending(i => i.Points).Take(10).ToList();
+		//	var highscores = (from h in UnitOfWork.TalentecheckHighscoreRepository.AsQueryable()
+		//		join
+		//			tc in UnitOfWork.TalentecheckSessionRepository.AsQueryable() on h.TalentecheckSessionId equals tc.Id
+		//		join user in UnitOfWork.BaseUserRepository.AsQueryable() on tc.UserId equals user.Id
 
-			var max = 0;
-			foreach (var item in highscores)
-			{
-				if (max == 0)
-				{
-					max = item.Points;
-				}
+		//		select new TalentecheckHighscoreItem()
+		//		{
+		//			Firstname = user.Firstname,
+		//			Surname = user.Surname,
+		//			Points = h.Points
+		//		}).OrderByDescending(i => i.Points).Take(10).ToList();
 
-				item.Rank = Convert.ToInt16(((double)item.Points/max)*10);
-			}
-			
-			resultPageData.HighscoreItems = highscores;
+		//	var max = 0;
+		//	foreach (var item in highscores)
+		//	{
+		//		if (max == 0)
+		//		{
+		//			max = item.Points;
+		//		}
 
-			return View(resultPageData);
-		}
+		//		item.Rank = Convert.ToInt16(((double)item.Points/max)*10);
+		//	}
 
-		public JsonResult AddShareBonus(string network)
-		{
-			UnitOfWork.TalentecheckSessionRepository.AddShareBonus(network, TalentecheckSession);
-			UnitOfWork.Save();
-			return Json(true, JsonRequestBehavior.AllowGet);
-		}
+		//	resultPageData.HighscoreItems = highscores;
+
+		//	return View(resultPageData);
+		//}
+
+		//public JsonResult AddShareBonus(string network)
+		//{
+		//	UnitOfWork.TalentecheckSessionRepository.AddShareBonus(network, TalentecheckSession);
+		//	UnitOfWork.Save();
+		//	return Json(true, JsonRequestBehavior.AllowGet);
+		//}
     }
 }
